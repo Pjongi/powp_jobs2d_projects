b164569fa71f056995618e95f0f6ecb07fc3d7c2
nina <ninazugaj25@gmail.com>
2026-04-08 11:12:05 +0200

    Update DriverManager.java

--- src/main/java/edu/kis/powp/jobs2d/drivers/DriverManager.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/DriverManager.java
@@ -7,7 +7,7 @@ import edu.kis.powp.observer.Publisher;
 /**
  * Driver manager provides means to setup the driver. It also enables other
  * components and features of the application to react on configuration changes.
- **/
+ */
 public class DriverManager {
 
     private Job2dDriver currentDriver = new LoggerDriver();
@@ -18,6 +18,7 @@ public class DriverManager {
      */
     public synchronized void setCurrentDriver(Job2dDriver driver) {
         currentDriver = driver;
+        changePublisher.notifyObservers();
     }
 
     /**
